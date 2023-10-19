e5719f68d0e1f272e4556dcf834bcda7990bc668
eryan <eryan.gauvrit@gmail.com>
2023-10-19 11:10:13 +0200

    First commit

---
+++ application_console/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace application_console
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ application_console/Platforms/Windows/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace application_console
+{
+    // All the code in this file is only included on Windows.
+    public class PlatformClass1
+    {
+    }
+}
